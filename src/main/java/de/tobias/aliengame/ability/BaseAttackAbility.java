9e3225b46f8b9a7487e0a9ca1fce053563699e8c
ichbintobias2 <t-gaming242@protonmail.com>
2021-04-11 14:51:49 +0200

    introducing BaseAttackAbility and HitEffect

--- src/main/java/de/tobias/aliengame/ability/BaseAttackAbility.java
+++ src/main/java/de/tobias/aliengame/ability/BaseAttackAbility.java
@@ -1,20 +1,55 @@
 package de.tobias.aliengame.ability;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.abilities.Ability;
+import de.gurkenlabs.litiengine.abilities.AbilityExecution;
+import de.gurkenlabs.litiengine.entities.ICombatEntity;
+import de.gurkenlabs.litiengine.util.geom.GeometricUtilities;
 import de.tobias.aliengame.GameLogic;
+import de.tobias.aliengame.effects.HitEffect;
 import de.tobias.aliengame.entities.Player;
 import de.tobias.aliengame.enums.Animations;
 import de.tobias.aliengame.enums.Gamestate;
 
-public class BaseAttackAbility {
+public class BaseAttackAbility extends Ability {
 	
 	private Player executor;
 	
+	private HitEffect hitEffect = new HitEffect(this);
+	
 	public BaseAttackAbility(Player executor) {
+		super(executor);
 		this.executor = executor;
 	}
 	
-	public void cast() {
+	@Override
+	public AbilityExecution cast() {
 		GameLogic.setGamestate(Gamestate.LOCKED);
 		executor.animations().play(Animations.PLAYER_ATTACK_RIGHT);
+		hitEffect.apply(findAffectedEnemy());
+
+		return super.cast();
+	}
+	
+	private ICombatEntity findAffectedEnemy() {
+		double dist = 0;
+		ICombatEntity closestInRange = null;
+		
+		List<ICombatEntity> enemies = Game.world().environment().getCombatEntities().stream()
+				.filter(en -> GeometricUtilities.shapeIntersects(en.getHitBox(), this.calculateImpactArea()))
+				.collect(Collectors.toList());
+		
+		for (ICombatEntity enemy : enemies) {
+			double newDist = enemy.getLocation().distance(this.getExecutor().getCenter());
+			if (closestInRange == null || newDist < dist) {
+				dist = newDist;
+				closestInRange = enemy;
+			}
+		}
+		
+		return closestInRange;
 	}
 }
---
+++ src/main/java/de/tobias/aliengame/effects/HitEffect.java
@@ -0,0 +1,42 @@
+package de.tobias.aliengame.effects;
+
+import java.awt.Color;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.abilities.Ability;
+import de.gurkenlabs.litiengine.abilities.effects.Effect;
+import de.gurkenlabs.litiengine.abilities.effects.EffectTarget;
+import de.gurkenlabs.litiengine.entities.ICombatEntity;
+import de.gurkenlabs.litiengine.entities.Prop;
+import de.gurkenlabs.litiengine.graphics.OverlayPixelsImageEffect;
+
+public class HitEffect extends Effect {
+	
+	public HitEffect(Ability ability) {
+		super(ability, EffectTarget.ENEMY);
+	}
+	
+	@Override
+	public void apply(final ICombatEntity affectedEntity) {
+		if (affectedEntity == null || affectedEntity.isIndestructible()) {
+			return;
+		}
+		
+		final int damage = this.getAbility().getAttributes().value().get();
+		super.apply(affectedEntity);
+		
+		affectedEntity.hit(damage, this.getAbility());
+		
+		if (!affectedEntity.isDead()) {
+			affectedEntity.animations().add(new OverlayPixelsImageEffect(120, new Color(255, 255, 255, 200)));
+			Game.loop().perform(130, () -> affectedEntity.animations()
+					.add(new OverlayPixelsImageEffect(120, new Color(167, 16, 16, 170))));
+		}
+	}
+	
+	@Override
+	protected boolean canAttackEntity(ICombatEntity entity) {
+		// cannot destroy props in LITI
+		return !(entity instanceof Prop) && super.canAttackEntity(entity);
+	}
+}
